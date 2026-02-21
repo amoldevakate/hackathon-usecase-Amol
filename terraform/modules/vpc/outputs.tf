a13e2a237fc2fbebf21f660482d97a99be9d92b6
amoldevakate <74484063+amoldevakate@users.noreply.github.com>
2026-02-21 12:09:16 +0530

    Update vpc module outputs.tf: add subnet, router, nat outputs

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,3 +1,54 @@
 output "vpc_id" {
-  value = google_compute_network.vpc.id
+  description = "The ID of the VPC network"
+  value       = google_compute_network.vpc.id
+}
+
+output "vpc_name" {
+  description = "The name of the VPC network"
+  value       = google_compute_network.vpc.name
+}
+
+output "vpc_self_link" {
+  description = "The self-link of the VPC network"
+  value       = google_compute_network.vpc.self_link
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = google_compute_subnetwork.public.id
+}
+
+output "public_subnet_name" {
+  description = "The name of the public subnet"
+  value       = google_compute_subnetwork.public.name
+}
+
+output "public_subnet_self_link" {
+  description = "The self-link of the public subnet"
+  value       = google_compute_subnetwork.public.self_link
+}
+
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = google_compute_subnetwork.private.id
+}
+
+output "private_subnet_name" {
+  description = "The name of the private subnet"
+  value       = google_compute_subnetwork.private.name
+}
+
+output "private_subnet_self_link" {
+  description = "The self-link of the private subnet"
+  value       = google_compute_subnetwork.private.self_link
+}
+
+output "router_name" {
+  description = "The name of the Cloud Router"
+  value       = google_compute_router.router.name
+}
+
+output "nat_name" {
+  description = "The name of the Cloud NAT"
+  value       = google_compute_router_nat.nat.name
 }
