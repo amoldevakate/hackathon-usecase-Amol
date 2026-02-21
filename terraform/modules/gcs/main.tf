29d4dcdce010b0ace600c7cc19fc774898f000cf
amoldevakate <74484063+amoldevakate@users.noreply.github.com>
2026-02-21 12:10:26 +0530

    Update gcs module main.tf: add project_id, storage class, lifecycle, labels, CORS, logging

--- terraform/modules/gcs/main.tf
+++ terraform/modules/gcs/main.tf
@@ -1,11 +1,57 @@
+# -----------------------------------------------------------------------
+# GCS Bucket: hcl-bucket-2026
+# -----------------------------------------------------------------------
 resource "google_storage_bucket" "bucket" {
-  name          = var.bucket_name
-  location      = var.location
-  force_destroy = true
+  project                     = var.project_id
+  name                        = var.bucket_name
+  location                    = var.location
+  storage_class               = var.storage_class
+  force_destroy               = var.force_destroy
+  uniform_bucket_level_access = true
+
+  labels = merge(
+    {
+      managed-by  = "terraform"
+      environment = "production"
+      project     = "hcltech-prod"
+    },
+    var.labels
+  )
 
   versioning {
-    enabled = true
+    enabled = var.versioning_enabled
   }
 
-  uniform_bucket_level_access = true
+  lifecycle_rule {
+    condition {
+      age = var.lifecycle_age_days
+    }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "NEARLINE"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age            = 90
+      with_state     = "ARCHIVED"
+      num_newer_versions = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  cors {
+    origin          = ["*"]
+    method          = ["GET", "HEAD"]
+    response_header = ["Content-Type"]
+    max_age_seconds = 3600
+  }
+
+  logging {
+    log_bucket        = var.bucket_name
+    log_object_prefix = "access-logs/"
+  }
 }
