9a67ec4a7055674e7e08736a7eb3233aab0aa1b2
amoldevakate <74484063+amoldevakate@users.noreply.github.com>
2026-02-21 12:57:49 +0530

    Update terraform/variables.tf: add GKE variables with defaults

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,7 @@ variable "project_id" {
 variable "region" {
   description = "Default GCP region for resources"
   type        = string
-  default     = "us-central1"
+  default     = "asia-south1"
 }
 
 # -----------------------------------------------------------------------
@@ -48,10 +48,10 @@ variable "private_cidr" {
 }
 
 # -----------------------------------------------------------------------
-# GCS Bucket Variables
+# GCS Variables
 # -----------------------------------------------------------------------
 variable "bucket_name" {
-  description = "Name of the GCS bucket (must be globally unique)"
+  description = "Name of the GCS bucket"
   type        = string
   default     = "hcl-bucket-2026"
 }
@@ -59,7 +59,7 @@ variable "bucket_name" {
 variable "bucket_location" {
   description = "Location for the GCS bucket"
   type        = string
-  default     = "US"
+  default     = "ASIA-SOUTH1"
 }
 
 variable "bucket_storage_class" {
@@ -69,16 +69,122 @@ variable "bucket_storage_class" {
 }
 
 variable "versioning_enabled" {
-  description = "Enable versioning on the GCS bucket"
+  description = "Enable versioning for the GCS bucket"
   type        = bool
   default     = true
 }
 
 variable "bucket_labels" {
-  description = "Additional labels to apply to the GCS bucket"
+  description = "Labels to apply to the GCS bucket"
   type        = map(string)
   default = {
-    team        = "hcltech"
-    cost-center = "hackathon"
+    environment = "production"
+    project     = "hcltech"
+    managed_by  = "terraform"
+  }
+}
+
+# -----------------------------------------------------------------------
+# GKE Variables
+# -----------------------------------------------------------------------
+variable "gke_cluster_name" {
+  description = "Name of the GKE cluster"
+  type        = string
+  default     = "hcltech-gke-cluster"
+}
+
+variable "node_service_account" {
+  description = "Service account email for GKE nodes"
+  type        = string
+  default     = "hcltech-terraform-sa@hcltech-prod.iam.gserviceaccount.com"
+}
+
+variable "master_ipv4_cidr_block" {
+  description = "CIDR block for GKE master network"
+  type        = string
+  default     = "172.16.0.0/28"
+}
+
+variable "cluster_secondary_range_name" {
+  description = "Secondary IP range name for pods"
+  type        = string
+  default     = "pods-range"
+}
+
+variable "services_secondary_range_name" {
+  description = "Secondary IP range name for services"
+  type        = string
+  default     = "services-range"
+}
+
+variable "master_authorized_networks" {
+  description = "Authorized networks to access GKE master"
+  type = list(object({
+    cidr_block   = string
+    display_name = string
+  }))
+  default = [
+    {
+      cidr_block   = "0.0.0.0/0"
+      display_name = "All (restrict for production)"
+    }
+  ]
+}
+
+variable "gke_release_channel" {
+  description = "GKE release channel (RAPID, REGULAR, STABLE)"
+  type        = string
+  default     = "REGULAR"
+}
+
+variable "node_pool_name" {
+  description = "Name of the GKE node pool"
+  type        = string
+  default     = "hcltech-node-pool"
+}
+
+variable "node_count" {
+  description = "Initial number of nodes per zone"
+  type        = number
+  default     = 1
+}
+
+variable "min_node_count" {
+  description = "Minimum number of nodes for autoscaling"
+  type        = number
+  default     = 1
+}
+
+variable "max_node_count" {
+  description = "Maximum number of nodes for autoscaling"
+  type        = number
+  default     = 3
+}
+
+variable "machine_type" {
+  description = "Machine type for GKE nodes"
+  type        = string
+  default     = "e2-standard-2"
+}
+
+variable "disk_size_gb" {
+  description = "Boot disk size in GB for GKE nodes"
+  type        = number
+  default     = 50
+}
+
+variable "node_tags" {
+  description = "Network tags for GKE nodes"
+  type        = list(string)
+  default     = ["gke-node", "hcltech-gke"]
+}
+
+variable "common_labels" {
+  description = "Common labels to apply to all resources"
+  type        = map(string)
+  default = {
+    environment = "production"
+    project     = "hcltech"
+    managed_by  = "terraform"
   }
 }
