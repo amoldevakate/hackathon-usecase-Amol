c37230eb2f597698e257c55b30ea4d78d17b183c
amoldevakate <74484063+amoldevakate@users.noreply.github.com>
2026-02-21 11:40:48 +0530

    Create main.tf

---
+++ terraform/modules/gcs/main.tf
@@ -0,0 +1,11 @@
+resource "google_storage_bucket" "bucket" {
+  name          = var.bucket_name
+  location      = var.location
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  uniform_bucket_level_access = true
+}
