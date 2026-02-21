5ed989bb284a7ea39ceb7a52c16872c68ad6538d
amoldevakate <74484063+amoldevakate@users.noreply.github.com>
2026-02-21 12:10:56 +0530

    Update gcs module outputs.tf: add url, self_link, location, storage_class outputs

--- terraform/modules/gcs/outputs.tf
+++ terraform/modules/gcs/outputs.tf
@@ -1,3 +1,24 @@
 output "bucket_name" {
-  value = google_storage_bucket.bucket.name
+  description = "The name of the GCS bucket"
+  value       = google_storage_bucket.bucket.name
+}
+
+output "bucket_url" {
+  description = "The base URL of the GCS bucket (gs://)"
+  value       = google_storage_bucket.bucket.url
+}
+
+output "bucket_self_link" {
+  description = "The URI of the GCS bucket"
+  value       = google_storage_bucket.bucket.self_link
+}
+
+output "bucket_location" {
+  description = "The location of the GCS bucket"
+  value       = google_storage_bucket.bucket.location
+}
+
+output "bucket_storage_class" {
+  description = "The storage class of the GCS bucket"
+  value       = google_storage_bucket.bucket.storage_class
 }
